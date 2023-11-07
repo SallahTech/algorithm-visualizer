be4342de32b98a06af53664e6bcc6c472ea4763f
Sallahtech <donlyrlmrktr@gmail.com>
2023-11-07 21:49:23 +0200

    fix routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import SortingVisualizer from "./pages/SortingPage/SortingVisualizer";
 import PathFindingVisualizer from "./pages/PathfindingPage/PathFindingVisualizer";
 import SideNav from "./components/sidenav/SideNav";
