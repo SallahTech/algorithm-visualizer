7884094a36c9044c42160b9adaab3a78ececf1ce
Sallah Baboucarr <agent47@Sallahs-MacBook-Air.local>
2024-06-23 10:22:00 +0300

    fix navigation

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from "react";
-import { HashRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import SortingVisualizer from "./pages/SortingPage/SortingVisualizer";
 import PathFindingVisualizer from "./pages/PathfindingPage/PathFindingVisualizer";
 import SideNav from "./components/sidenav/SideNav";
 import "./App.css";
 
 const App = () => {
-  // const [showNav, setShowNav] = useState(false);
-
   return (
     <div className="App">
       <Router>
         <SideNav isOpen={false} />
-        <Route path="/" exact component={SortingVisualizer} />
+        <Switch>
 
+        <Route path="/" exact component={SortingVisualizer} />
         <Route path="/searching" component={PathFindingVisualizer} />
+        </Switch>
       </Router>
     </div>
   );
